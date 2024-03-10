ceb5d0dda7dcdd12d06d414563be973afd35387f
MissStefie <mariah.stefanie.delga@gmail.com>
2024-03-10 19:30:29 -0300

    Ajuste de controllers.

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [InicioController::class, 'index']);
+Route::get('/', [VisitorController::class, 'index']);
 Route::post('/', [VisitorController::class, 'registrar'])->name('registrar');
